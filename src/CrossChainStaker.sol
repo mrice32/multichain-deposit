052f83b8a7270094401a5127aed0decf94ba03ad
Matt Rice <matthewcrice32@gmail.com>
2023-09-26 13:44:02 -0400

    contracts complete
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- src/CrossChainStaker.sol
+++ src/CrossChainStaker.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import {IAcrossMessageHandler} from "./interfaces/IAcrossMessageHandler.sol";
 import {IAcceleratingDistributor} from "./interfaces/IAcceleratingDistributor.sol";
 import {IHubPool} from "./interfaces/IHubPool.sol";
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract CrossChainStaker is IAcrossMessageHandler {
     IHubPool public immutable hubPool;
@@ -14,16 +15,16 @@ contract CrossChainStaker is IAcrossMessageHandler {
         acceleratingDistributor = _acceleratingDistributor;
     }
 
-    function handleAcrossMessage(
-        address tokenSent,
-        uint256 amount,
-        bool fillCompleted,
-        address relayer,
-        bytes memory message
-    ) external {}
+    function handleAcrossMessage(address tokenSent, uint256 amount, bool, address, bytes memory message) external {
+        (address userAddress) = abi.decode(message, (address));
+        (IERC20 lpToken, uint256 lpAmount) = _deposit(tokenSent, amount);
+        acceleratingDistributor.stakeFor(address(lpToken), lpAmount, userAddress);
+    }
 
-    function _deposit(address token, uint256 amount) private returns (address lpToken, uint256 amount) {
+    function _deposit(address token, uint256 depositAmount) private returns (IERC20 lpToken, uint256 lpAmount) {
         IHubPool.PooledToken memory pooledToken = hubPool.pooledTokens(token);
-        hubPool.addLiquidity(token, amount);
+        hubPool.addLiquidity(token, depositAmount);
+        lpToken = IERC20(pooledToken.lpToken);
+        lpAmount = lpToken.balanceOf(address(this));
     }
 }
